5610356e18ff80752b4ec050baef4880fc7fb1d3
S42296 <ikhmal.zubir@gmail.com>
2019-08-29 17:00:30 +0800

    Added buttons for links "Login" and "Register"

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,23 @@
 
         <title>LaraToDo</title>
 
+        <!--Link-->
+        <link rel="stylesheet" href="https://jqueryvalidation.org/files/demo/site-demos.css">
+        <link rel="stylesheet" 
+            href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+        <link rel='stylesheet' 
+            href='https://use.fontawesome.com/releases/v5.7.0/css/all.css' integrity='sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ' crossorigin='anonymous'>
+        <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+        <!--Script sources-->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+        <script src="https://jqueryvalidation.org/files/lib/jquery.js"></script>
+        <script src="https://jqueryvalidation.org/files/dist/jquery.validate.js"></script>
+        <script src="https://cdn.jsdelivr.net/jquery.validation/1.16.0/jquery.validate.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/jquery.validation/1.16.0/additional-methods.min.js"></script>
+        <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
@@ -66,8 +83,8 @@
     <body>
         <div class="flex-center position-ref full-height">
             <div class="top-right links">
-                <a href="{{ url('login') }}">Login</a><br/>
-                <a href="{{ url('register') }}">Register</a>
+                <button type="button" class="btn btn-info" onclick="location.href='{{ url('login') }}'">Login</button>&nbsp;
+                <button type="button" class="btn btn-warning" onclick="location.href='{{ url('register') }}'">Register</button>
             </div>
 
             <div class="content">
